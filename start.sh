63523cc577edf2f91639bdcb3d94a9486090da87
Apnamovi <99548630+Apnamovi@users.noreply.github.com>
2023-07-31 22:53:11 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Apnamovi/TopRepo /TopRepo
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /TopRepo
+fi
+cd /Elsa
+pip3 install -U -r requirements.txt
+echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖....🧞‍♂️"
+python3 bot.py
