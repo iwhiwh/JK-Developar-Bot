b7a9db9f84b1d9a6fc38dbdfe0489c268364f8ba
Apnamovi <99548630+Apnamovi@users.noreply.github.com>
2023-08-01 23:18:46 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -38,7 +38,7 @@ FILTER_MODE = {}
 
 @Client.on_message(filters.group | filters.private & filters.text & filters.incoming)
 async def give_filter(client, message):
-    if message.chat.id != SUPPORT_CHAT_ID:
+    if message.chat.id != SUPPORT_CHAT:
         manual = await manual_filters(client, message)
         if manual == False:
             settings = await get_settings(message.chat.id)
